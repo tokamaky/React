a9ae3fa51f4efc58393514de8409798e0d25175c
Xiaohang Ji <jxh186045@gmail.com>
2023-08-17 21:23:11 -0400

    update file for mails

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
-const mailjet = require('node-mailjet');
+const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
-
 
 const Survey = mongoose.model('surveys');
 
@@ -14,43 +13,30 @@ module.exports = (app) => {
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
-  
+
     const survey = new Survey({
       title,
       subject,
       body,
       recipients: recipients
         .split(',')
-        .map((email) => ({ Email: email.trim() })),
+        .map((email) => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now(),
     });
-  
-    // Send email using Mailjet
-    const mailjetClient = mailjet.apiConnect('keys.mailjetPublicKey', 'keys.mailjetPrivateKey');
-    const request = mailjetClient.post('send', { version: 'v3.1' }).request({
-      Messages: [
-        {
-          From: {
-            Email: 'no-reply@emaily.com',
-          },
-          To: survey.recipients,
-          Subject: survey.subject,
-          HTMLPart: surveyTemplate(survey),
-        },
-      ],
-    });
-  
+
+    // Great place to send an email!
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
     try {
-      await request;
+      await mailer.send();
       await survey.save();
       req.user.credits -= 1;
       const user = await req.user.save();
-  
+
       res.send(user);
     } catch (err) {
       res.status(422).send(err);
     }
   });
-  
 };
